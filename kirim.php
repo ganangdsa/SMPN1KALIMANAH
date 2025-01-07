7c03550669aa16eca928af2c282023aac6dddcaa
ganangdsa <ganangsetya80@gmail.com>
2025-01-08 03:45:09 +0700

    Update kirim.php
    Signed-off-by: ganangdsa <ganangsetya80@gmail.com>

---
+++ kirim.php
@@ -0,0 +1,25 @@
+<?php
+// Koneksi ke database
+$servername = "localhost";
+$username = "root"; // Sesuaikan dengan username database Anda
+$password = ""; // Sesuaikan dengan password database Anda
+$dbname = "smpn1kalimanah";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Cek koneksi
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Cek apakah form telah disubmit
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nama = $_POST['name'];
+    $email = $_POST['email'];
+    $subjek = $_POST['subject'];
+    $pesan = $_POST['message'];
+
+    // Menyimpan data ke dalam tabel kontak
+    $sql = "INSERT INTO kontak (nama_lengkap, email, subjek, pesan) VALUES ('$nama', '$email', '$subjek', '$pesan')";
+
+    if ($conn->query($sql) ==…
